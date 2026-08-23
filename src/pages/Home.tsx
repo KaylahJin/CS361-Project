c2f621e1eceb855c36bb624c577da0185583a625
Aungaing <mintita.kul@gmail.com>
2026-08-23 18:26:36 +0700

    Setup React TypeScript

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+function Home() {
+  return (
+    <main className="home">
+      <h1 className="home-title">
+        Cooperative Education
+      </h1>
+
+      <h2 className="home-subtitle">
+        Planning & Management System
+      </h2>
+
+      <p className="home-thai">
+        ระบบจัดการแผน<span>สหกิจศึกษา</span>
+      </p>
+    </main>
+  );
+}
+
+export default Home;
